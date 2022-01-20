986de77a7a46d44d877e38e8cc259be8be980ce2
VnioDilol <dilolvnio@gmail.com>
2022-01-20 14:23:54 +0800

    remake map sittacsis

--- main.py
+++ main.py
@@ -10,13 +10,26 @@ class MAP:
 		self.color = (120,120,120)
 		self.screen = app.sc
 		self.map_text = (
-			'#######',
-			'#.....#',
-			'#.....#',
-			'#..#..#',
-			'#..#..#',
-			'#..#..#',
-			'#######'
+			'#########################',
+			'#.......................#',
+			'#.......................#',
+			'#..#....................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#.......................#',
+			'#..#....................#',
+			'#########################'
 			)
 		self.map_pos = list()
 
@@ -24,15 +37,15 @@ class MAP:
 		for num_row, row in enumerate(self.map_text):
 			for num_char, char in enumerate(row):
 				if char == '#':
-					self.map_pos.append((num_char * 25,num_row * 25))
+					self.map_pos.append((num_char * (wd / 25),num_row * (hi / 20)))
 		#print(self.map_pos)
 	
 	def draw(self):
 		for pos in self.map_pos:
-			pygame.draw.rect(self.screen,self.color,(* pos,25,25),2) 
-			pygame.draw.rect(self.screen,'green',(player.x / 4 - 5, player.y / 4 - 5, 10, 10),1) 
-			#print(pos)
-
+			pygame.draw.rect(self.screen,self.color,(* pos,30,30),2) 
+			pygame.draw.rect(self.screen,'green',(player.x - 10, player.y - 10, 20, 20),1) 
+			pygame.draw.line(self.screen,'green',(player.x,player.y), (400,300)  )
+			#print(pos)))
 class App:
 	def __init__(self):
 		self.sc = pygame.display.set_mode(res)
